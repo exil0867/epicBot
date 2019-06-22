05dd42f5058165e559279285be8f3af184c5e248
ExillustX <mrexillustx@gmail.com>
2019-06-22 16:05:36 +0100

    fix mistype

--- modules/activeUsers/interval.js
+++ modules/activeUsers/interval.js
@@ -16,13 +16,13 @@ exports.run = async () => {
     const userId = user.user_id;
     const userTag = user.user_tag;
     // If user started being active
-    if ((beingActiveSince !== null) && (dateNow.diff(moment(beingActiveSince), 'days') >= env.process.DAYS_TO_GET_ACTIVE_ROLE) && (hasActiveRoleSince === null)) {
+    if ((beingActiveSince !== null) && (dateNow.diff(moment(beingActiveSince), 'days') >= process.env.DAYS_TO_GET_ACTIVE_ROLE) && (hasActiveRoleSince === null)) {
       console.log(`Adding the active role to the user: ${userTag}: ${userId}`);
       manageActiveRole('add', process.env.SERVER_ID, userId, process.env.ACTIVE_ROLE_ID);
       table.update({ has_active_role_since: dateNow.format('YYYY-MM-DD') }, { where: { user_id: userId } });
     }
     // If user was active but then stopped being active
-    if ((beingActiveSince !== null) && (dateNow.diff(moment(lastTimeBeingActive), 'days') >= env.process.DAYS_TO_LOSE_ACTIVE_ROLE)) {
+    if ((beingActiveSince !== null) && (dateNow.diff(moment(lastTimeBeingActive), 'days') >= process.env.DAYS_TO_LOSE_ACTIVE_ROLE)) {
       console.log(`Removing the active role from the user: ${userTag}: ${userId}`);
       manageActiveRole('remove', process.env.SERVER_ID, process.env.ACTIVE_ROLE_ID);
       table.update({ has_active_role_since: dateNow.format('YYYY-MM-DD'), being_active_since: null }, { where: { user_id: userId } });
