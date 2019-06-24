dfb4a6cdf18c21d522928c1e87535d0b13c7d355
ExillustX <mrexillustx@gmail.com>
2019-06-24 14:02:19 +0100

    improve the logic of the function

--- modules/activeUsers/functions.js
+++ modules/activeUsers/functions.js
@@ -2,27 +2,27 @@ const functions = {
   manageActiveRole: (action, serverId, memberId, roleId) => {
     const member = client.guilds.get(serverId).members.get(memberId);
     if (action === 'add') {
-      if (!member.roles.has(roleId)) {
-        member.addRole(roleId).then(() => {
-          console.log('Added the role to the user');
-        }).catch((error) => {
-          console.error(error);
-        });
-      } else {
-        console.log('The user already has the role!');
+      if (member.roles.has(roleId)) {
+        console.log(`The user: ${member.user.tag}: ${member.user.id} already has the role!`);
+        return;
       }
+      member.addRole(roleId).then(() => {
+        console.log(`Added the role to the user: ${member.user.tag}: ${member.user.id}`);
+      }).catch((error) => {
+        console.error(error);
+      });
     }
     if (action === 'remove') {
-      if (member.roles.has(roleId)) {
-        member.removeRole(roleId).then(() => {
-          console.log('Removed the role from the user!');
-        }).catch((error) => {
-          console.log(error);
-          console.log(`Couldn't remove the role from the user!`);
-        });
-      } else {
-        console.log(`The user already doesn't have the role!`);
+      if (!member.roles.has(roleId)) {
+        console.log(`The user: ${member.user.tag}: ${member.user.id} already doesn't have the role!`);
+        return;
       }
+      member.removeRole(roleId).then(() => {
+        console.log(`Removed the role from the user ${member.user.tag}: ${member.user.id}`);
+      }).catch((error) => {
+        console.log(error);
+        console.log(`Couldn't remove the role from the user ${member.user.tag}: ${member.user.id}`);
+      });
     }
   }
 }
