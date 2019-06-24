8b01e4fa4550dfc26ae027676c984074eb84f3c2
ExillustX <mrexillustx@gmail.com>
2019-06-25 00:57:52 +0100

    disable logging

--- modules/activeUsers/sequelize.js
+++ modules/activeUsers/sequelize.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  logging: false
+});
 
 module.exports = sequelize;
