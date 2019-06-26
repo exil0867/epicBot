9aec81e32a6403b0ef7b34ad3d7378d8e34e3181
ExillustX <mrexillustx@gmail.com>
2019-06-26 13:32:25 +0100

    emotes-only channel feature

--- index.js
+++ index.js
@@ -68,6 +68,22 @@ client.on('message', message => {
 
 });
 
+client.on('message', message => {
+  if (message.author.bot || !message.guild) {
+    return;
+  }
+  if (message.channel.id == process.env.EMOTES_ONLY_CHANNEL) {
+    if (!((message.content.startsWith('<a:') || message.content.startsWith('<:')) && message.content.endsWith('>'))) {
+      message.delete();
+      message.channel.send('Your message has been deleted! This channel is in emotes-only mode. <:peeshifar:537055607144841216>').then((botMessage) => {
+        botMessage.delete(5000);
+      }).catch(error => {
+        console.log(error);
+      });
+    }
+  }
+});
+
 // client.on('message', message => {
 //   if (message.author.bot || !message.guild) {
 //     return;
