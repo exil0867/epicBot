e1f8f394c0708a995c67e91cdbff9b4b4386d90d
ExillustX <mrexillustx@gmail.com>
2019-07-04 18:46:58 +0100

    fix outdated code

--- modules/emotes-only/index.js
+++ modules/emotes-only/index.js
@@ -5,9 +5,17 @@ exports.run = async (client, message) => {
     return;
   }
   if (message.channel.id == process.env.EMOTES_ONLY_CHANNEL) {
-    if (!((message.content.startsWith('<a:') || message.content.startsWith('<:')) && message.content.endsWith('>'))) {
+    if (message.author.id == client.user.id) {
+      return;
+    }
+    if (message.author.bot) {
       message.delete();
-      message.channel.send('Your message has been deleted! This channel is in emotes-only mode. <:peeshifar:537055607144841216>').then((botMessage) => {
+      return;
+    }
+    const text = message.content.replace(/:[^:\s]+:|<:[^:\s]+:[0-9]+>|<a:[^:\s]+:[0-9]+>/g, '').replace(/\s+/g, '');
+    if (text) {
+      message.delete();
+      message.channel.send(`<@${message.member.user.id}> Your message has been deleted! This channel is in emotes-only mode. <:peeshifar:537055607144841216>`).then((botMessage) => {
         botMessage.delete(5000);
       }).catch(error => {
         console.log(error);
